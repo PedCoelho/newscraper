fdb105bbde481791c91489ebef9f671ee5310352
pedro.silva <pedro.silva@mjv.com.br>
2021-02-11 21:13:05 -0300

    filteredNews

--- Scraper/CurrentNews.js
+++ Scraper/CurrentNews.js
@@ -5,6 +5,7 @@ const AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 let uniqueNews;
+
 (async function scrape(attempt, maxAttempts) {
   const requestUrl =
     'https://oglobo.globo.com/api/v1/ultimas-noticias/ece_frontpage/ece_frontpage/conteudo.json?';
@@ -75,64 +76,92 @@ let uniqueNews;
   if (noticias) {
     uniqueNews = [...noticias.reduce((m, t) => m.set(t.titulo, t), new Map()).values()];
 
-    let forPrint = uniqueNews
-      .map((x, i) => {
-        return `
-      <div class="card">
-        <div class="title">
-          <h1>${i}</h1>
-          <p>Publicado em: ${new Date(x.publicadoEm).toLocaleString()}
-          </p>
-        </div>
-        <div class="main-section">
-          <div>
-            <img src="${x.imagens.length > 0 ? x.imagens[0].url : 'noimage-found.png'}">
-          </div>
-          <div>
-            <b>Categoria: ${x.secao.nome}</b><br>
-            <h2>${x.titulo}</h2>
-            <h4>${x.subTitulo}</h4>
-            <p>Endereço:</p>
-            <a class="link" href="${x.url}">${x.url}</a>
-          </div>
-        </div>
-      </div>
-      `;
-      })
-      .join('');
+    let filters = [
+      'Esportes',
+      'Filmes',
+      'Ela',
+      'ComÃ©rcio em pauta',
+      'Gastronomia',
+      'OpiniÃ£o',
+      'TV',
+      'Cultura',
+      'content.include.diversos',
+      'Rio Gastronomia',
+      'Bairros',
+      'Boa Viagem',
+      'Viagem',
+      'Rio Show',
+    ];
 
-    const header = `<!DOCTYPE html>
-      <html lang="pt-BR">
-      
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Noticias JORNAL OGLOBO - 24h</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="shortcut icon" href="favicon.ico" />
-      </head>
-      
-      <body>
-        <div class="image-hider"><img src="./icon.png"></div>
-        `;
+    const filteredNews = uniqueNews.filter(
+      (el) => !filters.some((word) => el.secao.nome.toUpperCase() == word.toUpperCase())
+    );
+    const excludedNews = uniqueNews.filter((el) =>
+      filters.some((word) => el.secao.nome.toUpperCase() == word.toUpperCase())
+    );
+    const excludedCount = uniqueNews.length - filteredNews.length;
+    const newsCount = filteredNews.length;
 
-    const closing = `<script>
-      document.querySelector('.image-hider').addEventListener('click', toggleImages)
-  
-      function toggleImages() {
-        let images = document.querySelectorAll('.main-section div:first-child')
-        images.forEach((div) => {
-          div.toggleAttribute('hidden')
-          div.style.flex = '0 0 0 100%'
-        })
-  
-        let otherDiv = document.querySelectorAll('.main-section div:last-child')
-        otherDiv.forEach((div) => div.classList.toggle('full-flex'))
-      }
-    </script>
-  </body>
-  
-  </html>`;
+    const obj = { excludedCount, newsCount, data: filteredNews, excluded: excludedNews };
+
+    //   let forPrint = uniqueNews
+    //     .map((x, i) => {
+    //       return `
+    //     <div class="card">
+    //       <div class="title">
+    //         <h1>${i}</h1>
+    //         <p>Publicado em: ${new Date(x.publicadoEm).toLocaleString()}
+    //         </p>
+    //       </div>
+    //       <div class="main-section">
+    //         <div>
+    //           <img src="${x.imagens.length > 0 ? x.imagens[0].url : 'noimage-found.png'}">
+    //         </div>
+    //         <div>
+    //           <b>Categoria: ${x.secao.nome}</b><br>
+    //           <h2>${x.titulo}</h2>
+    //           <h4>${x.subTitulo}</h4>
+    //           <p>Endereço:</p>
+    //           <a class="link" href="${x.url}">${x.url}</a>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     `;
+    //     })
+    //     .join('');
+
+    //   const header = `<!DOCTYPE html>
+    //     <html lang="pt-BR">
+
+    //     <head>
+    //       <meta charset="UTF-8">
+    //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //       <title>Noticias JORNAL OGLOBO - 24h</title>
+    //       <link rel="stylesheet" href="style.css">
+    //       <link rel="shortcut icon" href="favicon.ico" />
+    //     </head>
+
+    //     <body>
+    //       <div class="image-hider"><img src="./icon.png"></div>
+    //       `;
+
+    //   const closing = `<script>
+    //     document.querySelector('.image-hider').addEventListener('click', toggleImages)
+
+    //     function toggleImages() {
+    //       let images = document.querySelectorAll('.main-section div:first-child')
+    //       images.forEach((div) => {
+    //         div.toggleAttribute('hidden')
+    //         div.style.flex = '0 0 0 100%'
+    //       })
+
+    //       let otherDiv = document.querySelectorAll('.main-section div:last-child')
+    //       otherDiv.forEach((div) => div.classList.toggle('full-flex'))
+    //     }
+    //   </script>
+    // </body>
+
+    // </html>`;
 
     // console.log(uniqueNews);
 
@@ -145,7 +174,7 @@ let uniqueNews;
     var objectParams = {
       Bucket: bucketName,
       Key: keyName,
-      Body: JSON.stringify(uniqueNews),
+      Body: JSON.stringify(obj),
     };
     // Create object upload promise
     var uploadPromise = new AWS.S3().putObject(objectParams).promise();
